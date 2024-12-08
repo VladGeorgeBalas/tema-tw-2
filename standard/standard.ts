52868bb50c122c0e37c52692bdd0d35fc94568f2
vladb <vlad_george.balas@stud.acs.upb.ro>
2024-12-09 00:51:50 +0200

    comm

--- standard/standard.ts
+++ standard/standard.ts
@@ -2,10 +2,24 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("menu_button")?.addEventListener("click", function (event) {
         event.stopImmediatePropagation();
         if (event.target instanceof Element) {
-            if(event.target.getAttribute("state") === "unclicked")
+            if(event.target.getAttribute("state") === "unclicked") {
                 event.target.setAttribute("state", "clicked");
-            else
+                event.target.textContent = "<<<"
+            }
+            else {
                 event.target.setAttribute("state", "unclicked");
+                event.target.textContent = "///"
+            }
         }
     });
+
+    if(document.documentElement != null) {
+        if (window.innerWidth < window.innerHeight) {
+            document.documentElement.style.setProperty("--menu-width", "100vw");
+        } else {
+            document.documentElement.style.setProperty("--menu-width", "10vw");
+        }
+    }
+    else
+        console.log("not supported");
 });
